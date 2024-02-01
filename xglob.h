e34ffe257d2ed27d0cfbf3a61a78527d5fc0985c
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-02-01 08:32:15 +0200

    sync

---
+++ xglob.h
@@ -0,0 +1,25 @@
+#ifndef __xglob_h__
+#define __xglob_h__
+
+#include <zephyr/kernel.h>
+
+#define EVAL(s) s
+#define SPREAD(...) __VA_ARGS__
+#define ARGY(s, ...) s
+
+#define SKIP(s, ...) __VA_ARGS__
+#define SKIP0(...) ARGY(__VA_ARGS__)
+#define SKIP1(...) SKIP(__VA_ARGS__)
+#define SKIP2(...) SKIP(SKIP(__VA_ARGS__))
+#define SKIP3(...) SKIP(SKIP(SKIP(__VA_ARGS__)))
+#define SKIP4(...) SKIP(SKIP(SKIP(SKIP(__VA_ARGS__))))
+#define SKIP5(...) SKIP(SKIP(SKIP(SKIP(SKIP(__VA_ARGS__)))))
+
+#define ARGN(n, ...) ARGY(SKIP##n(__VA_ARGS__))
+#define STRY(s, ...) #s
+#define STRINGY(n) STRY(n)
+
+#define mem_alloc k_malloc
+#define mem_free k_free
+
+#endif
